44d05168e5377c0e0282ba80bf4ed2507418bfe1
dwatek <damian.watroba.cc@gmail.com>
2021-04-18 13:26:31 +0200

    Initial commit

---
+++ lib/ruby_shopping_cart.rb
@@ -0,0 +1,6 @@
+require "ruby_shopping_cart/version"
+
+module RubyShoppingCart
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ruby_shopping_cart/version.rb
@@ -0,0 +1,3 @@
+module RubyShoppingCart
+  VERSION = "0.1.0"
+end
